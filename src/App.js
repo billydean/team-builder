af574a40cbf40e453b6923148b343b03976f5e83
billydean <billy.goehring@gmail.com>
2022-02-22 11:28:28 -0800

    got ducks in row for new Form component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+// setting up initial form values to use later
+const initialValues = {
+  name: "",
+  email: "",
+  role: ""
+}
 function App() {
+// tracking two different slices of state: list of team members and values submitted by form component
+  const [team, setTeam] = useState([]);
+  const [values, setValues] = useState(initialValues);
+// function for updating form values 
+  const update = (name, value) => {
+    setValues({ ...values, [name]: value });
+  };
+// function for submitting form values to update member list
+  const submit = () => {
+    const member = {
+      name: values.name.trim(),
+      email: values.email.trim(),
+      role: values.role
+    };
+    setTeam([...team, member]);
+    setValues(initialValues);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Form 
+      
+      /> */}
+      {team.forEach(member => {
+        return (
+          <div>
+            <h3>{member.name}</h3>
+            <p>{member.role}</p>
+            <p>{member.email}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
